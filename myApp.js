cda65c71a9bda53ccb46899b4a374b7de83b8031
Nimesin <info@nimesin.com>
2022-01-04 14:03:00 +0100

    static route to /public

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ var app = express();
 
 console.log("Hello World");
 
+app.use("/public", express.static( __dirname + '/public'));
+
 app.get("/", function(req, res) {
 	//res.send('Hello Express');
 	var path = __dirname + "/views/index.html";
